77197d9903b01d761c28ef7109969ee03edfafc1
Victor <gorodkov.victor@gmail.com>
2023-09-19 12:29:00 +0300

    new wordCheck that includes all useCases

--- src/utils/handleWordAnimation.ts
+++ src/utils/handleWordAnimation.ts
@@ -30,3 +30,34 @@ export const handleWordAnimation = (
     }
   }
 };
+
+export const handleWord = (word: string, target: string, colors: SharedValue<string>[]) => {
+  word = word.toLowerCase();
+  target = target.toLowerCase();
+  const correctIndicies: number[] = [];
+  let newTarget = target;
+  //find correctLetters first.
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === target[i]) {
+      correctIndicies.push(i);
+      colors[i].value = withTiming(Theme.colors.correctLetter, {
+        duration: ANIMATION_DURATION * i,
+      });
+      newTarget = newTarget.replace(word[i], 's'); //remove from target correct letters and remain it with the same length.
+    }
+  }
+  //check other letters wether they inWord. //For example, target is СЯБАР and СЕМ'Я is the word. Prevent from last Я marked as inWordWord. While СAСНА target and КАЗАК word second A will be marked as inWord.
+  for (let i = 0; i < word.length; i++) {
+    if (!correctIndicies.includes(i)) {
+      if (newTarget.includes(word[i])) {
+        colors[i].value = withTiming(Theme.colors.inWordLetter, {
+          duration: ANIMATION_DURATION * i,
+        });
+      } else if (!correctIndicies.includes(i)) {
+        colors[i].value = withTiming(Theme.colors.notInWordLetter, {
+          duration: ANIMATION_DURATION * i,
+        });
+      }
+    }
+  }
+};
