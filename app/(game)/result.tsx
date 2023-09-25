114bb334e12417e066d0e2529b1e25eefea82767
Victor <gorodkov.victor@gmail.com>
2023-09-25 16:22:23 +0300

    link to word definition

--- app/(game)/result.tsx
+++ app/(game)/result.tsx
@@ -1,6 +1,6 @@
 import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-
+import * as Linking from 'expo-linking';
 import { router, useLocalSearchParams } from 'expo-router';
 
 const Result = () => {
@@ -16,6 +16,12 @@ const Result = () => {
       <View style={styles.btnContainer}>
         <Pressable style={styles.btn} onPress={() => router.replace('/')}>
           <Text style={styles.txt}>У меню</Text>
+        </Pressable>
+        <Pressable
+          style={styles.btn}
+          onPress={() => Linking.openURL(`https://ru.wiktionary.org/wiki/${target}`)}
+        >
+          <Text style={styles.txt}>Значэнне слова</Text>
         </Pressable>
         <Pressable style={styles.btn} onPress={() => router.replace('/main')}>
           <Text style={styles.txt}>Далей</Text>
@@ -38,17 +44,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 16,
     marginTop: 16,
+    justifyContent: 'space-between',
   },
   btn: {
     borderWidth: 1,
     padding: 12,
+    backgroundColor: 'rgba(0,0,0,0.6)',
     borderColor: 'white',
+
     borderRadius: 4,
   },
   targetWord: {
     textTransform: 'uppercase',
-    fontSize: 16,
-    fontWeight: '600',
+    fontSize: 20,
+    fontWeight: '700',
   },
   txt: {
     color: 'white',
