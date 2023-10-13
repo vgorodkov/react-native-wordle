95d36396c081b53d0f265b4ff66b098425b7e11e
Victor <gorodkov.victor@gmail.com>
2023-10-13 16:03:44 +0300

    new colors

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -1,8 +1,10 @@
 export const Theme = {
   colors: {
+    initialLetter: 'transparent',
     correctLetter: '#5B9279',
     inWordLetter: '#FF8552',
     notInWordLetter: '#161616',
+    incorrectLetter: '#FE5F55',
     disabled: '#3E3E3E',
     primary: '#F6E7BE',
   },
